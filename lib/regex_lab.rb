b4b5810eef5f2e80288c8e3b7586c0a2bbb3e40d
spensation <spencevans@gmail.com>
2020-02-10 12:41:49 -0500

    three methods working

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+	word.match(/\b[^aeiou]/i) ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+	text.scan(/\bun\w+ing/).flatten
 end
 
 def words_five_letters_long(text)
-
+	text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
